336de8e56a08830475d934aa385d7eed8ec882d2
msolivera <32486643+msolivera@users.noreply.github.com>
2022-06-11 15:55:59 -0300

    Se crea proyecto ANGULAR

---
+++ frontend/src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+
+
+@NgModule({
+  declarations: [RegisterComponent, LoginComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    AuthRoutingModule
+  ],
+  exports: [LoginComponent, RegisterComponent]
+})
+export class AuthModule { }
