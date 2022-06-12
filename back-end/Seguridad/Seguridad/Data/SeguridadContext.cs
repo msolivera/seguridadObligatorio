f3632d88d224b61f671723deb39edf99b613a0e3
DanielSarasolaPioli <56884981+DanielSarasolaPioli@users.noreply.github.com>
2022-06-12 19:03:32 -0300

    API primera versión.

---
+++ back-end/Seguridad/Seguridad/Data/SeguridadContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Seguridad.Models;
+
+namespace Seguridad.Data
+{
+    public class SeguridadContext : DbContext
+    {
+        public SeguridadContext (DbContextOptions<SeguridadContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Seguridad.Models.User> User { get; set; }
+    }
+}
